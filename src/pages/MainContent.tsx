eca83ab264b952ad8984633a236a1dbbf8906381
apurupachintala95 <89081384+apurupachintala95@users.noreply.github.com>
2025-10-13 20:53:20 +0530

    push

--- src/pages/MainContent.tsx
+++ src/pages/MainContent.tsx
@@ -688,7 +688,7 @@ const MainContent = ({
                     sx={{
                       display: "flex",
                       // justifyContent: "flex-start", // Align to left
-                      justifyContent: message.fromUser ? "flex-end" : "flex-start",
+                      justifyContent: message.fromUser ? "flex-start" : "flex-end",
                     }}
                   >
                     {message.fromUser ? (
